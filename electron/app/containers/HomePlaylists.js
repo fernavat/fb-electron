0cc1e70dfc9b8e5e626cbd106f01288103d53c12
fernavat <fernavat@gmail.com>
2018-08-06 11:31:58 -0600

    app renamed

---
+++ electron/app/containers/HomePlaylists.js
@@ -0,0 +1,14 @@
+// @flow
+// 
+import React, { Component } from 'react';
+import Playlists from '../components/Playlists';
+
+type Props = {};
+
+export default class HomePlaylists extends Component<Props> {
+  props: Props;
+
+  render() {
+    return <Playlists />;
+  }
+}
---
+++ electron/app/routes.js
@@ -0,0 +1,17 @@
+/* eslint flowtype-errors/show-errors: 0 */
+import React from 'react';
+import { Switch, Route } from 'react-router';
+import App from './containers/App';
+import HomePage from './containers/HomePage';
+import HomePlaylists from './containers/HomePlaylists';
+import CounterPage from './containers/CounterPage';
+
+export default () => (
+  <App>
+    <Switch>
+      <Route path="/counter" component={CounterPage} />
+      <Route path="/home" component={HomePage} />
+      <Route path="/" component={HomePlaylists} />
+    </Switch>
+  </App>
+);
